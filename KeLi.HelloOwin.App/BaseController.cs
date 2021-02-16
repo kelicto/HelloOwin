33f53d172230e4bd34518ca065b360fcd7285642
KeLi <kelicto@protonmail.com>
2021-02-16 23:01:10 +0800

    Initials solution code.

---
+++ KeLi.HelloOwin.App/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace KeLi.HelloOwin.App
+{
+    public abstract class BaseController<T> : ApiController
+    {
+        public abstract void Post([FromBody]string value);
+
+        public abstract void Delete(int id);
+
+        public abstract void Put(int id, [FromBody] string value);
+
+        public abstract T Get(int id);
+
+        public abstract List<T> Get();
+    }
+}
